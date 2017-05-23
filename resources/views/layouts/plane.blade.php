8ff7a439c1c9e00c8fbddbd0c93fefdb966fee01
kpandian110 <kpandian110@gmail.com>
2017-05-23 17:43:23 +0530

    Add files via upload

--- resources/views/layouts/plane.blade.php
+++ resources/views/layouts/plane.blade.php
@@ -6,16 +6,34 @@
 <!--<![endif]-->
 <head>
 	<meta charset="utf-8"/>
-	<title>SB Admin v2.0 in Laravel 5</title>
+	<title>Skylink Fibernet Private Ltd</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta content="width=device-width, initial-scale=1" name="viewport"/>
 	<meta content="" name="description"/>
 	<meta content="" name="author"/>
 
-	<link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" />
+	<link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" /> 
+	<link rel="stylesheet" href="{{ asset("assets/stylesheets/style.css")  }}">
 </head>
-<body>
+<body style="overflow-x: hidden;">
+	<div class="row">
+	<header>
+	<div style="background-color:#534d64;height:100px;">
+	</header>
+	</div>
+	
+	
+
 	@yield('body')
-	<script src="{{ asset("assets/scripts/frontend.js") }}" type="text/javascript"></script>
+
+	<script src="{{ asset("assets/scripts/frontend.js") }}" type="text/javascript"></script> 
+	
+<footer>
+		<div class="row">
+		<div style="background-color: #534d64;height: 75px;">
+		<p style="text-align: center;color:#ffffff;"> Powered By Skylink Fibernet Private Ltd </p>
+		</div>
+		</div>
+	</footer>
 </body>
 </html>
